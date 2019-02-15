d73348aa8c300d8b557bd858c9907e444f4a7447
melvinabraham <melvin.abraham1996@gmail.com>
2019-02-15 15:38:03 -0700

    Added initial microservice

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -0,0 +1,20 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.List;
+import java.util.Arrays;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("spring", "Spring Framework", "Spring Framework Description"),
+				new Topic("java", "Core Java", "Core Java Description"),
+				new Topic("javascript", "JavaScript", "Spring Framework Description")
+				);
+	}
+
+}
