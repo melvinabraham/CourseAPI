879f2b13d14457a685aacd6a223d26e51e4f8223
melvinabraham <melvin.abraham1996@gmail.com>
2019-02-23 00:18:55 -0700

    Added REST API for POST Request for a topic

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +24,11 @@ public class TopicController {
 	@RequestMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/topics") 
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.add(topic);
 	}
 	
 }
