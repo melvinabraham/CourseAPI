53a8495c1d954ea7cdb05333088c6b17be6538ca
melvinabraham <melvin.abraham1996@gmail.com>
2019-03-18 20:09:34 -0700

    Added DELETE REST Call

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -30,10 +30,15 @@ public class TopicController {
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.add(topic);
 	}
-//	
-//	@RequestMapping(method=RequestMethod.PUT,value="/topics/{id}") 
-//	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
-//		topicService.updateTopic(id, topic);
-//	}
+	
+	@RequestMapping(method=RequestMethod.PUT,value="/topics/{id}") 
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE,value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+	 topicService.deleteTopic(id);
+	}
 	
 }
--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -36,5 +36,10 @@ public class TopicService {
 			 }
 		 }
 	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t->t.getId().equals(id));
+		return;
+	}
 	
 }
