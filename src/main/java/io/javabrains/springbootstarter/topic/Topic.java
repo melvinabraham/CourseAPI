cd0e8cd3cb18582cd24b07ea78c0c87cddcc43d3
melvinabraham <melvin.abraham1996@gmail.com>
2019-03-21 19:06:45 -0700

    Added application.properties and DELETE Request

--- src/main/java/io/javabrains/springbootstarter/topic/Topic.java
+++ src/main/java/io/javabrains/springbootstarter/topic/Topic.java
@@ -1,7 +1,11 @@
 package io.javabrains.springbootstarter.topic;
 
-public class Topic {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Topic {
+	@Id 
 	private String id;
 	private String name;
 	private String description;
